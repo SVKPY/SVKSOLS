6080a991cb6c19c65db6dbda286c815d74b4e543
SVKPY <saikumar.mdsk@gmail.com>
2018-04-18 15:34:04 -0400

    added todo model

--- svkproject/svkapp/models.py
+++ svkproject/svkapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+# Create your models here
+class Todo(models.Model):
+    name = models.CharField(primary_key=True, max_length=36)
+    desc = models.CharField(max_length=36)
+    topic = models.CharField(max_length=36)
+
